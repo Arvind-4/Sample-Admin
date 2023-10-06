95eb81274d934e725bb1f8ff032fcc3597ba4b85
Arvind <lordarvindthegamer@gmail.com>
2023-10-06 11:41:27 +0000

    Updated: root level files

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
+import ECommerce from './pages/Homepage';
 import Portfolio from './pages/Portfolio';
 
 function App() {
@@ -32,23 +23,14 @@ function App() {
   }, []);
 
   return loading ? (
-    <p className=" text-center text-danger">Failed to lead app</p>
+    <p className=' text-center text-danger'>Failed to lead app</p>
   ) : (
     <>
       <Routes>
-        <Route path="/" element={<ECommerce />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/forms/form-elements" element={<FormElements />} />
-        <Route path="/forms/form-layout" element={<FormLayout />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/chart" element={<Chart />} />
-        <Route path="/ui/alerts" element={<Alerts />} />
-        <Route path="/ui/buttons" element={<Buttons />} />
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
+        <Route path='/' element={<ECommerce />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/auth/signin' element={<SignIn />} />
+        <Route path='/auth/signup' element={<SignUp />} />
       </Routes>
     </>
   );
